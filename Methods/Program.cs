f0009215b62e681993346d94fd3ad67b85ad6340
Camille <camille@salvinien.com>
2021-02-01 14:16:19 -0500

    Commit

---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hi! What is your name?");
+
+            try
+            {
+                string userName = Console.ReadLine();
+                nameOutput(userName);
+            }
+
+            catch
+            {
+                Console.WriteLine("You must enter your name.");
+                Console.WriteLine("Press any key to exit the program and try again.");
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void nameOutput(string userName)
+            {
+
+            Console.WriteLine("Hello" + userName + " !");
+
+            }
+    }
+}
